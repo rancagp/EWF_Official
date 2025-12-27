10b898fe88ffdb4f0531d981c3f37ef79068b59e
kresno46 <kresnonugroho46@gmail.com>
2025-12-28 04:20:17 +0700

    update:firebase

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,26 +1,58 @@
-import { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "G-4FYTRTD1KR";
 
 export default function Document() {
   // Default language
-  const lang = 'id';
-  
+  const lang = "id";
+
   return (
     <Html lang={lang}>
       <Head>
+        {/* ✅ Google Tag (gtag.js) */}
+        <Script
+          strategy="beforeInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <Script id="gtag-init" strategy="beforeInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
+
         {/* Favicon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
         <link rel="manifest" href="/favicon/site.webmanifest" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <meta name="theme-color" content="#ffffff" />
-        
+
         {/* Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
       </Head>
+
       <body className="antialiased">
         <Main />
         <NextScript />
